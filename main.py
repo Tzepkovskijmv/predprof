a1d009a1f0357ea2bbf56f62c4e20fd927a3be5f
tsepkovskiimv <max.tzepkovskij@yandex.ru>
2024-02-07 17:33:40 +0300

    not working cod

--- main.py
+++ main.py
@@ -1,2 +1,31 @@
-print(100+5)
-print(100+1)
+class studentiki:
+    id = 0
+    fio = ''
+    if_project = ''
+    clas = ''
+    score = 0
+studens = []
+f = open('students.csv')
+j = 0
+skip = f.readline()
+for i in f:
+    s=i.split(',')
+    if int(s[3][:-1]) == 10:
+        studens.append(studentiki())
+        studens[j].fio = s[1]
+        studens[j].score = int (s[4])
+        j += 1
+
+for i in range(len(studens)-1):
+    j = i
+    t = studens[i]
+    st = studens[i].score
+    sid=studens[i].id
+    while j>0 and studens[j-1].score>st and studens[j-1].id>sid:
+        studens[j] = studens[j-1]
+        j -= 1
+    studens[j] = t
+
+print(studens[0].fio)
+print(studens[1].fio)
+print(studens[2].fio)
